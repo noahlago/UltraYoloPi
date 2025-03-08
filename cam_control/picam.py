c96ec0c42a12ed08d862b905bd97a9269c0830c8
noahlago <ndl3389@rit.edu>
2025-03-08 12:23:50 -0500

    Completed camera initialization

--- cam_control/picam.py
+++ cam_control/picam.py
@@ -1,2 +1,12 @@
 import cv2
 from picamera2 import Picamera2
+from ultralytics import YOLO 
+
+#Camera initialization
+picam = Picamera2()
+picam.preview_configuration.main.size = (1280, 720)
+picam.preview_configuration.main.format = "RGB888"
+picam.preview_configuration.align
+picam.configure("preview")
+picam.start
+
