270d9c731e87ac96faf7e6cff75b6c4121688ba2
noahlago <ndl3389@rit.edu>
2025-04-02 22:44:00 -0400

    updated backend to support tracking of the 10 most recently processed images and results

--- yolo_webserver/server.js
+++ yolo_webserver/server.js
@@ -7,13 +7,24 @@ const PORT = 3000;
 //Static server files (HTML, CSS, JS)
 app.use(express.static('public'));
 
-//Fetch gesture and image data from the python yolo api
-app.get('/gesture', async (req, res) => {
+//Fetch the detected gesture
+app.get('/gesture', async (res) => {
     try {
         const response = await axios.get('http://localhost:5000/gesture');
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ error: "Failed to fetch gesture data" });
+    }
+});
+
+//Fetch the latest 10 images with attached gesture detections
+app.get('/recent', async (res) => {
+    try{
+        const response = await axios.get('http://localhost:5000/recent');
+        res.json(response.data);
+    }
+    catch (error){
+        res.status(500).json({ error: "Failed to fetch recent images" });
     }
 });
 
