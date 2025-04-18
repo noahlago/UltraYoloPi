bbd3c2a56e0173d2a8196887ef099282c926c7ea
noahlago <ndl3389@rit.edu>
2025-04-18 12:50:31 -0400

    removed tutorial/trial code. All needed functionality moved to yolo_pi_server

--- demos/benchmark.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a YOLO11n PyTorch model
-model = YOLO("yolo11n.pt")
-
-# Benchmark YOLO11n speed and accuracy on the COCO8 dataset for all all export formats
-results = model.benchmark(data="coco8.yaml", imgsz=640)
